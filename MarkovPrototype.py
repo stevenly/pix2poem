63ada1725189b015fe597ed6407fb98ff3507c0b
Kevin <kevin.karabinas@gmail.com>
2017-04-05 16:42:49 -0700

    Add files via upload

--- MarkovPrototype.py
+++ MarkovPrototype.py
@@ -6,53 +6,67 @@ EOS = ['.', '?', '!',',']
 
 
 def build_dict(words):
-    """
-    Build a dictionary from the words.
-
-    (word1, word2) => [w1, w2, ...]  # key: tuple; value: list
-    """
+    '''Build a Dictionary from words
+    (word1, word2) -> [w1,w2,...] # key: tuple; value: list
+    '''
     d = {}
     for i, word in enumerate(words):
+        #tries to set all three words for trigram
         try:
             first, second, third = words[i], words[i + 1], words[i + 2]
+        #breaks if end of file
         except IndexError:
             break
+        #sets key as first, second tuple
         key = (first, second)
+        #all keys are initialized with a empty list
         if key not in d:
             d[key] = []
-        #
+        #adds third gram to key's list
         d[key].append(third)
 
     return d
 
 
 def generate_sentence(d,begin):
+    '''
+    :param d: generated dictionary
+    :param begin: boolean stating whether this is the beginning of the stanza or not
+    :return: returns sentence/line
+    '''
+    #if beginning of sentence, create list of tuples with capitalized first word
     if begin:
-        li = [key for key in d.keys() if key[0][0].isupper()]
+        line = [key for key in d.keys() if key[0][0].isupper()]
+    #if not begin, create list of tuples with no capitalized first word
     else:
-        li = [key for key in d.keys() if key[0][0].islower()]
+        line = [key for key in d.keys() if key[0][0].islower()]
 
+    #loop to ensure that [! or ? or , or .] are not present in key
     EOSdetected = True
     while EOSdetected:
-        key = choice(li)
+        key = choice(line)
         first, second = key
 
         if first[-1] in EOS or second[-1] in EOS:
             EOSdetected = True
         else:
             EOSdetected = False
-    li = []
-    li.append(first)
-    li.append(second)
-    #print(first)
-    #print(second)
+    line = []
+    line.append(first)
+    line.append(second)
+
+    '''
+        True loop to continue adding to sentence
+        *If tuple has no third, end sentence creation
+        *if sentence has more than 7 words, end sentence creation
+    '''
     while True:
         try:
             third = choice(d[key])
         except KeyError:
             break
-        li.append(third)
-        if len(li) > 7:
+        line.append(third)
+        if len(line) > 7:
             break
         if third[-1] in EOS:
             break
@@ -60,16 +74,23 @@ def generate_sentence(d,begin):
         key = (second, third)
         first, second = key
 
-    return ' '.join(li)
+    return ' '.join(line)
 
+
+#Main
+#import text from file
 fname = 'justpoems.txt'
 with open(fname, "rt", encoding="utf-8") as f:
     text = f.read()
 
+#splits text into words
 words = text.split()
+
+#builds dictionary from words
 d = build_dict(words)
 #pprint(d)
-#print()
+
+
 for x in range(4):
     if x == 0:
         sent = generate_sentence(d,True)
